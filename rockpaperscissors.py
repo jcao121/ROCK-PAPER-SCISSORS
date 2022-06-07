af40864f66235d9c5e3fc9d80bd4ca19acc4e6fe
jcao121 <jcao121@gmail.com>
2022-06-07 08:51:50 -0700

    Added f-string and scores

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -1,27 +1,30 @@
-
-game_score = 0
-while game_score < 3:
-    player1 = input("Rock, Paper or Scissors: ").lower()
+player1_score = 0  # I wanted to keep score so added variable, need to use while loop for these variables to be useful
+player2_score = 0
+while player1_score != 3 and player2_score != 3:
+    # had issue with while loop not working because I kept setting conditions that were true so it kept running
+    # while loop runs until it is false so needed to do opposite
+    player1 = input("Rock, Paper or Scissors: ").lower() # added user input and .lower so it would not be case-sensitive
     player2 = input("Rock, Paper or Scissors: ").lower()
 
     if player1 == "rock" and player2 == "scissors":
-        print("Player 1 Wins")
-        game_score += 1
-        break
+        player1_score += 1
+        print(f"PLAYER 1 WINS and has {player1_score} point") # Added F-string to keep track of points
     elif player1 == "paper" and player2 == "rock":
-        print("Player 1 wins")
-        game_score += 1
+        player1_score += 1
+        print(f"PLAYER 1 WINS and has {player1_score} point")
     elif player1 == "scissors" and player2 == "paper":
-        print("Player 1 wins")
-        game_score += 1
+        player1_score += 1
+        print(f"PLAYER 1 WINS and has {player1_score} point")
     elif player2 == "rock" and player1 == "scissors":
-        print("Player 2 Wins")
-        game_score += 1
+        player2_score += 1
+        print(f"PLAYER 2 WINS and has {player2_score} point")
     elif player2 == "paper" and player1 == "rock":
-        print("Player 2 wins")
-        game_score += 1
+        player2_score += 1
+        print(f"PLAYER 2 WINS and has {player2_score} point")
     elif player2 == "scissors" and player1 == "paper":
-        print("Player 2 wins")
-        game_score += 1
+        player2_score += 1
+        print(f"PLAYER 2 WINS and has {player2_score} point")
+    elif player1 == player2:
+        print("ITS A TIE! WE GO AGAN")
     else:
         print("Unrecognized Option")
